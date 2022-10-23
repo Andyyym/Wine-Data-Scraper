29515d8279b267dea7f67b98a88a42bef58f2771
Andyyy <42917091+Andyyym@users.noreply.github.com>
2022-10-23 15:28:36 +0200

    Version 2 update: rework on server

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const PORT = process.env.PORT || 8000;
 const express = require('express');
 
-const Wine = require('./Data/RedWine.json');
-const WoolWorths = require('./Data/Woolworths.json');
-const PicknPay = require('./Data/PicknPay.json');
-const Makro = require('./Data/Makro.json');
+const Wine = require('./Data/WineData.json');
 
 const app = express()
 
@@ -13,20 +10,9 @@ app.get('/',(req,res) =>{
     res.json('welcome to wine api')
 })
 
-app.get('/store/', (req,res) => {
+app.get('/redwine', (req,res) => {
     res.json(Wine)
 })
 
-app.get('/store/woolworths', (req,res) => {
-    res.json(WoolWorths)
-})
-
-app.get('/store/picknpay', (req,res) => {
-    res.json(PicknPay)
-})
-
-app.get('/store/makro', (req,res) => {
-    res.json(Makro)
-})
 
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`))
