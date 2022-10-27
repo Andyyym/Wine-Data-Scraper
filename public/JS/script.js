07c90b9160627e73b01a8bd27e8e6f87b2400dcf
Andyyy <42917091+Andyyym@users.noreply.github.com>
2022-10-27 03:23:17 +0200

    Web Redesign

--- public/JS/script.js
+++ public/JS/script.js
@@ -9,8 +9,26 @@ $(document).ready(function(){
       $.each(data, function(key, value){
        if (value.Name.search(expression) != -1)
        {
-        $('#result').append('<li class="list-group-item link-class">'+value.Name+' \n| '+value.Price+' | \n<span class="text-muted">'+value.Deal+'</span> | <a href='+value.URL+'>'+value.Store+'</a>');
-       }
+       
+         // $('#result').append('<li class="list-group-item link-class">'+value.Name+' \n| '+value.Price+' | \n<span class="text-muted">'+value.Deal+'</span> | <a href='+value.URL+'>'+value.Store+'</a>');
+         
+         $('#result').append(`
+         <div class="row">
+         <div class="col s12">
+           <div class="card  grey darken-4 darken-1">
+             <div class="card-content white-text">
+               <h4 class="card-title m1">${value.Name} (${
+              value.Price
+            })<span class="blue-text m-4"> ${value.Deal}</span></h4>
+            <div class="card-action">
+            <a href=${value.URL}>${value.Store}</a>
+          </div>
+             </div>
+           </div>
+         </div>
+       </div>
+         `) 
+      }
       });   
      });
     });
