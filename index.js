02bd502f1077fcfb8d5272e0f4c85aaf7dca6810
Andyyy <42917091+Andyyym@users.noreply.github.com>
2023-01-10 13:00:48 +0200

    New Data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // Define Express App
 const app = express();
-const PORT = process.env.PORT || 80;
+// const PORT = process.env.PORT || 80;
 // const { spawn } = require('child_process');
 
 // Serve Static Assets
@@ -12,6 +12,6 @@ app.use(express.static('images'));
 // const bat = spawn('cmd.exe', ['run.bat']);
 
 app.use(express.static('public'));
-app.listen(PORT, () => {
-    console.log('Server connected at:', PORT);
+app.listen(3030, () => {
+    console.log('Server connected at: 3030');
 });
